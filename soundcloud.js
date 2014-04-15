dd4fd63d4c5df6dea31b9b3a04046c2c634efff3
Cameron J Roe <cameronjroe@gmail.com>
2014-04-15 15:56:22 -0400

    added support for soundcloud

---
+++ soundcloud.js
@@ -0,0 +1,42 @@
+(function () {
+
+	var SoundCloud = {
+
+		api: {
+			key: '83c37277b2c82f9b59b3ebbae1fd7de3',
+			secret: 'secret',
+			redirect_uri: 'http://localhost:8000/callback.html'
+		},
+
+		init: function () {
+
+		    SC.initialize({
+		      client_id: SoundCloud.api.key,
+		      redirect_uri: SoundCloud.api.redirect_uri
+		    });
+
+		    SoundCloud.bindings();
+
+		},
+
+		bindings: function () {
+
+			this.loginBtn = document.querySelector('.soundcloud-login');
+			this.loginBtn.addEventListener('click', this.logIn);
+
+		},
+
+		logIn: function () {
+			// initiate auth popup
+			SC.connect(function() {
+				SC.get('/me', function(me) {
+					console.log(me);
+				});
+			});
+		}
+
+	};
+
+	window.onload = SoundCloud.init;
+
+})();
